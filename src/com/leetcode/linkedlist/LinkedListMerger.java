1c89dc32af75d73a9b871150e8dfbc36986777f4
Sushanth Bangera <itsushanth@gmail.com>
2020-05-14 02:14:21 +0530

    Improvised linked lst merge

--- src/com/leetcode/linkedlist/LinkedListMerger.java
+++ src/com/leetcode/linkedlist/LinkedListMerger.java
@@ -8,45 +8,32 @@ public class LinkedListMerger {
 
 	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
-		ListNode mergedNode = null;
+		ListNode mergedNode = new ListNode(0); // dummyHead
 		ListNode newNode = null;
-		ListNode currentNode = null;
+		ListNode currentNode = mergedNode;
 
-		while (l1 != null && l2 != null) {
+		while (l1 != null || l2 != null) {
 
-			if (l1.val <= l2.val) {
+			if (l1 != null && l2 != null) {
+				if (l1.val <= l2.val) {
+					newNode = new ListNode(l1.val);
+					l1 = l1.next;
+				} else {
+					newNode = new ListNode(l2.val);
+					l2 = l2.next;
+				}
+			} else if (l1 != null) {
 				newNode = new ListNode(l1.val);
 				l1 = l1.next;
 			} else {
 				newNode = new ListNode(l2.val);
 				l2 = l2.next;
 			}
-
-			if (mergedNode != null) {
-				currentNode.next = newNode;
-				currentNode = currentNode.next;
-			} else {
-				mergedNode = newNode;
-				currentNode = newNode;
-			}
+			
+			currentNode.next = newNode;
+			currentNode = currentNode.next;
 		}
 
-		if (l1 != null || l2 != null) {
-			l1 = l1 != null ? l1 : l2;
-		}
-
-		while (l1 != null) {
-			newNode = new ListNode(l1.val);
-			if (mergedNode != null) {
-				currentNode.next = newNode;
-				currentNode = currentNode.next;
-			} else {
-				mergedNode = newNode;
-				currentNode = newNode;
-			}
-			l1 = l1.next;
-		}
-
-		return mergedNode;
+		return mergedNode.next; // skip the dummy head node
 	}
 }
