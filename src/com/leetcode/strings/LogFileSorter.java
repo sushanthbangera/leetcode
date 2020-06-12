bf2325c64af69040000c0d8b2ad212426683c562
Sushanth Bangera <itsushanth@gmail.com>
2020-06-12 23:53:12 +0530

    Refactorinng

--- src/com/leetcode/strings/LogFileSorter.java
+++ src/com/leetcode/strings/LogFileSorter.java
@@ -3,7 +3,6 @@ package com.leetcode.strings;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import com.leetcode.utils.ArrayUtils;
