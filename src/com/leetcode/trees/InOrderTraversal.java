e24f9cb7229aba64b2e24b512ec8e67b4db5b465
Sushanth Bangera <itsushanth@gmail.com>
2020-07-05 10:01:06 +0530

    Tree Algos

---
+++ src/com/leetcode/trees/InOrderTraversal.java
@@ -0,0 +1,29 @@
+package com.leetcode.trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * 94. Binary Tree Inorder Traversal
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/
+ */
+public class InOrderTraversal {
+
+	/*
+	 * Time: O(n) Space: O(n)
+	 */
+	public List<Integer> inorderTraversal(TreeNode root) {
+		List<Integer> list = new ArrayList<>();
+		inOrderTraversal(root, list);
+		return list;
+	}
+
+	private void inOrderTraversal(TreeNode root, List<Integer> list) {
+		if (root == null)
+			return;
+
+		inOrderTraversal(root.left, list);
+		list.add(root.val);
+		inOrderTraversal(root.right, list);
+	}
+}
