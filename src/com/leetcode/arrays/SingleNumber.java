48435e519259e68cc0f084030035950dd2f163fc
Sushanth Bangera <itsushanth@gmail.com>
2020-07-19 16:38:20 +0530

    Array Algos

---
+++ src/com/leetcode/arrays/SingleNumber.java
@@ -0,0 +1,78 @@
+package com.leetcode.arrays;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class SingleNumber {
+
+	/*
+	 * Time: O(N) and Space: O(N)
+	 */
+	public int singleNumber(int[] nums) {
+
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int n : nums) {
+			if (map.containsKey(n)) {
+				map.remove(n);
+			} else {
+				map.put(n, 1);
+			}
+		}
+		// only one element would remain in the map
+		return map.keySet().iterator().next();
+	}
+
+	/*
+	 * Time: O(N) and Space: O(N)
+	 */
+	public int singleNumber2(int[] nums) {
+
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int n : nums) {
+			map.put(n, map.getOrDefault(n, 0) + 1);
+		}
+
+		for (Integer key : map.keySet()) {
+			if (map.get(key) == 1) {
+				return key;
+			}
+		}
+		return -1;
+	}
+
+	/*
+	 * 2*(a + b + c) - (2a + 2b + c) = c Time: O(N) Space: O(N)
+	 */
+	public int singleNumber3(int[] nums) {
+
+		Set<Integer> set = new HashSet<>();
+		int uniqueNumbersSum = 0;
+		int sequenceSum = 0;
+
+		for (int n : nums) {
+			if (!set.contains(n)) {
+				set.add(n);
+				uniqueNumbersSum += n;
+			}
+			sequenceSum += n;
+		}
+
+		return 2 * uniqueNumbersSum - sequenceSum;
+	}
+
+	/*
+	 * XOR with same numbers reduces to 0 Time: O(N) Space: O(1)
+	 */
+	public int singleNumber4(int[] nums) {
+		int result = 0;
+
+		for (int n : nums) {
+			result = result ^ n;
+		}
+		return result;
+	}
+}
---
+++ src/com/leetcode/arrays/TwoSum.java
@@ -0,0 +1,29 @@
+package com.leetcode.arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * 1. Two Sum
+ * https://leetcode.com/problems/two-sum/
+ */
+public class TwoSum {
+
+	/*
+	 * Time: O(N) Space: O(N)
+	 */
+	public int[] twoSum(int[] nums, int target) {
+		int len = nums.length;
+
+		Map<Integer, Integer> indexMap = new HashMap<>();
+
+		for (int i = 0; i < len; i++) {
+			int coplement = target - nums[i];
+			if (indexMap.containsKey(coplement)) {
+				return new int[] { i, indexMap.get(coplement) };
+			}
+			indexMap.put(nums[i], i);
+		}
+		throw new IllegalArgumentException("No Two Sum Found");
+	}
+}
