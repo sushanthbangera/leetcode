ab89efe44511d3ff40a81651ccd43e33886d3d60
Sushanth Bangera <itsushanth@gmail.com>
2020-07-26 12:28:59 +0530

    Updated min stack with Optimized approach

--- src/com/leetcode/stack/MinStack.java
+++ src/com/leetcode/stack/MinStack.java
@@ -38,4 +38,36 @@ public class MinStack {
 	public int getMin() {
 		return minStack.peek();
 	}
+
+	/*
+	 * ------------- Using only on stack -----------
+	 */
+
+	int min = Integer.MAX_VALUE;
+	Stack<Integer> stack2 = new Stack<Integer>();
+
+	public void push2(int x) {
+		// push the old minimum value when the current
+		// minimum value changes after with the new value x
+		if (x <= min) {
+			stack.push(min);
+			min = x;
+		}
+		stack.push(x);
+	}
+
+	public void pop2() {
+		// if pop operation could result in the changing of the current minimum value,
+		// pop twice and change the current minimum value to the last minimum value.
+		if (stack.pop() == min)
+			min = stack.pop();
+	}
+
+	public int top2() {
+		return stack.peek();
+	}
+
+	public int getMin2() {
+        return min;
+	}
 }
