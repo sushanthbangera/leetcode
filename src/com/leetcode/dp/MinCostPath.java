e025a75314ecf773125f49b7e7b695ed053bceac
Sushanth Bangera <itsushanth@gmail.com>
2020-08-06 22:55:58 +0530

    Updating variabled for better understanding

--- src/com/leetcode/dp/MinCostPath.java
+++ src/com/leetcode/dp/MinCostPath.java
@@ -16,15 +16,17 @@ public class MinCostPath {
 
 		int dp[][] = new int[grid.length][grid[0].length];
 
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid[0].length; j++) {
-				dp[i][j] = grid[i][j];
-				if (i > 0 && j > 0) {
-					dp[i][j] += Math.min(dp[i - 1][j], dp[i][j - 1]);
-				} else if (i > 0) {
-					dp[i][j] += dp[i - 1][j];
-				} else if (j > 0) {
-					dp[i][j] += dp[i][j - 1];
+		for (int row = 0; row < grid.length; row++) {
+			for (int col = 0; col < grid[0].length; col++) {
+				
+				dp[row][col] = grid[row][col];
+				
+				if (row > 0 && col > 0) {
+					dp[row][col] += Math.min(dp[row - 1][col], dp[row][col - 1]);
+				} else if (row > 0) {
+					dp[row][col] += dp[row - 1][col];
+				} else if (col > 0) {
+					dp[row][col] += dp[row][col - 1];
 				}
 			}
 		}
